30ff9280aa340e97608c5ca2d01003493f9359e4
Fatima Zahare <fz.elhammoumi@gmail.com>
2018-01-31 21:30:51 +0000

    manage users:signup v1

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -7,38 +7,124 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\View\ViewHandler;
 use FOS\RestBundle\View\View;
 use AppBundle\Document\User;
+use AppBundle\Entity\AuthToken;
 use AppBundle\Document\Shop;
 
 class UserController extends Controller
 {
+
     /**
-     * @Get("/api/users")
+     * @Rest\View(statusCode=Response::HTTP_CREATED, serializerGroups={"token"})
+     * @Rest\Get("/api/signup")
      */
-    public function getPlacesAction(Request $request)
+    public function signupAction(Request $request)
     {
-        return new JsonResponse([
-            new User("email1@mail.fr", "dfgdgdgsdfsfsdfsff"),
-            new User("email2@mail.fr", "sfsfsfsfsfsf"),
-            new User("email3@mail.fr", "sdffffffffffffffffffdfsffs"),
-        ]);
+
+      $user = new User();
+
+      $data = $request->request->all();
+      if (($request->request->method == "POST") && !empty($data)) {
+
+          $user->setEmail($data->email);
+          $encoder = $this->get('security.password_encoder');
+          // le mot de passe en claire est encodé avant la sauvegarde
+          $encoded = $encoder->encodePassword($user, $data->password);
+          $user->setPassword($encoded);
+
+          $em = $this->get('doctrine_mongodb');
+          $em->persist($user);
+          $em->flush();
+
+          //Generate Token
+          $authToken = new AuthToken();
+          $authToken->setValue(base64_encode(random_bytes(50)));
+          $authToken->setCreatedAt(new \DateTime('now'));
+          $authToken->setUser($user);
+
+          $em->persist($authToken);
+          $em->flush();
+
+          return $authToken;
+      } else {
+          return array('error' => 'errororr' );
+      }
+
+
+      $dm = $this->get('doctrine_mongodb')->getManager();
+      $dm->persist($shop);
+      $dm->flush();
+
+        $apiKey = $request->query->get('apikey');
+        $credentials = new Credentials();
+        $form = $this->createForm(CredentialsType::class, $credentials);
+
+        $form->submit($request->request->all());
+
+        if (!$form->isValid()) {
+            return $form;
+        }
+
+        $em = $this->get('doctrine_mongodb');
+
+        $user = $em->getRepository('AppBundle:User')
+            ->findOneByEmail($credentials->getLogin());
+
+        if (!$user) { // L'utilisateur n'existe pas
+            return $this->invalidCredentials();
+        }
+
+        $encoder = $this->get('security.password_encoder');
+        $isPasswordValid = $encoder->isPasswordValid($user, $credentials->getPassword());
+
+        if (!$isPasswordValid) { // Le mot de passe n'est pas correct
+            return $this->invalidCredentials();
+        }
+
+        $authToken = new AuthToken();
+        $authToken->setValue(base64_encode(random_bytes(50)));
+        $authToken->setCreatedAt(new \DateTime('now'));
+        $authToken->setUser($user);
+
+        $em->persist($authToken);
+        $em->flush();
+
+        return $authToken;
     }
 
     /**
-     * @Get("/api/create")
+     * @Rest\Get("/api/signin")
+     */
+    public function signinAction()
+    {
+        $shops = $this->get('doctrine_mongodb')
+            ->getRepository('AppBundle:Shop')
+            ->findAll();
+
+        if (!$shops) {
+            throw $this->createNotFoundException('No shop found for id ');
+        }
+       $viewHandler = $this->get('fos_rest.view_handler');
+
+       $view = View::create($shops);
+       $view->setFormat('json');
+
+       return $viewHandler->handle($view);
+    }
+
+    private function invalidCredentials()
+    {
+        return \FOS\RestBundle\View\View::create(['message' => 'Invalid credentials'], Response::HTTP_BAD_REQUEST);
+    }
+    /**
+     * @Rest\Get("/api/create")
      */
     public function createAction()
     {
-      // $user = new User();
-      // $user->setEmail('email2@mail.fr');
-      // $user->setPassword('sdfffffffDDDfffffffffffdfsffs');
-      //
-      // $dm = $this->get('doctrine_mongodb')->getManager();
-      // $dm->persist($user);
-      // $dm->flush();
 
       $shop = new Shop();
       $shop->setEmail('email33@mail.fr');
@@ -55,7 +141,7 @@ class UserController extends Controller
     }
 
     /**
-     * @Get("/api/show")
+     * @Rest\Get("/api/show")
      */
     public function showAction()
     {
@@ -70,39 +156,4 @@ class UserController extends Controller
         return new JsonResponse($shops);
     }
 
-    /**
-     * @Get("/api/signup")
-     */
-    public function signupAction()
-    {
-        $shops = $this->get('doctrine_mongodb')
-            ->getRepository('AppBundle:Shop')
-            ->findAll();
-
-        if (!$shops) {
-            throw $this->createNotFoundException('No shop found for id '.$id);
-        }
-
-        return new JsonResponse($shops);
-    }
-
-    /**
-     * @Get("/api/signin")
-     */
-    public function signinAction()
-    {
-        $shops = $this->get('doctrine_mongodb')
-            ->getRepository('AppBundle:Shop')
-            ->findAll();
-
-        if (!$shops) {
-            throw $this->createNotFoundException('No shop found for id '.$id);
-        }
-       $viewHandler = $this->get('fos_rest.view_handler');
-
-       $view = View::create($shops);
-       $view->setFormat('json');
-
-       return $viewHandler->handle($view);
-    }
 }
